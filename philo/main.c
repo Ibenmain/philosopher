57692b3a58b0ac87f471ca6d0bb3b11e178c8827
Issam Benmaina <ibenmain@e3r2p10.1337.ma>
2022-06-07 10:45:19 +0100

    push

--- philo/main.c
+++ philo/main.c
@@ -6,13 +6,36 @@
 /*   By: ibenmain <ibenmain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 09:47:18 by ibenmain          #+#    #+#             */
-/*   Updated: 2022/06/07 09:47:18 by ibenmain         ###   ########.fr       */
+/*   Updated: 2022/06/07 10:44:08 by ibenmain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+#include "philo.h"
+
+int	ft_cheak_error(char **av)
 {
-    int a=0;
-    
-	
+	int	i;
+	int	j;
+
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (av[i][j] > '9' || av[i][j] < '0')
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if ((argc != 5 && argc != 6) || ft_cheak_error(argv))
+		printf("Invalid arguments");
+
+	return (0);
 }
---
+++ philo/philo.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibenmain <ibenmain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/07 09:58:26 by ibenmain          #+#    #+#             */
+/*   Updated: 2022/06/07 10:41:52 by ibenmain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+int	ft_cheak_error(char **av);
+#endif
